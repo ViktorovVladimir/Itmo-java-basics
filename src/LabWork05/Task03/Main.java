2c6298f23388791babe7240fad62707b4da943c2
v-bat <167399035+ViktorovVladimir@users.noreply.github.com>
2024-12-28 13:02:10 +0300

    - removed the hardcode and put it in the parameters. To be able to use the method for different cases.

--- src/LabWork05/Task03/Main.java
+++ src/LabWork05/Task03/Main.java
@@ -2,16 +2,16 @@ package LabWork05.Task03;
 
 public class Main {
 
-    private static String biakaReplace(String text) {
+    private static String biakaReplace(String text, String patternSearch, String patternReplace) {
 
-        return text.replaceAll("(?i)бяка", "[вырезано цензурой]");
+        return text.replaceAll(patternSearch, patternReplace);
     }
 
     public static void main(String[] args) {
 
         String text = "Тысячи волонтеров бяка вручную Бяка собирают мазут бяка бяка бяка на пляжах, отмывают от него собак бяка, дельфинов Бяка бяка и птиц БЯКА! и просят власти дать технику хотя бы для вывоза мусора бяка бяка. Нефтепродукты тают бяка в мешках бяка бяка и обратно вытекают бякабякабякана песок.";
 
-        System.out.println(biakaReplace(text));
+        System.out.println(biakaReplace(text, "(?i)бяка", "[вырезано цензурой]"));
     }
 
 }
